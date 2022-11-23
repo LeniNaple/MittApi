ed091aaeba4e80c677049b9d48b60e98ca21503b
Jesper <jesperkun@gmail.com>
2022-11-23 22:35:38 +0100

    new stuff

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,13 +2,15 @@ const express = require('express')
 const controller = express.Router()
 let users = require("../data/simulated_database")
 
-
+controller.param("id", (req, res, next, id) => {
+    req.user = users.find(user => user.id == id)
+    next()
+})
 
 // POST - CREATE - SKAPA ANVÄNDARE
 // http://localhost:5000/api/users
-controller.post('/', (httpRequest, httpResponse) => {
-    
-
+controller.route('/')
+.post((httpRequest, httpResponse) => {
     let user = {
         id: (users[users.length -1])?.id > 0 ? (users[users.length -1])?.id + 1 : 1, 
         firstName: httpRequest.body.firstName,
@@ -16,17 +18,44 @@ controller.post('/', (httpRequest, httpResponse) => {
         email: httpRequest.body.email,
         password: httpRequest.body.password
     }
-
     users.push(user)
-    httpResponse.status(201).json(users)
+    httpResponse.status(201).json(user)
 })
-
-// GET - READ - HÄMTA ALLA ANVÄNDARE
-// http://localhost:5000/api/users
-controller.get('/', (httpRequest, httpResponse) => {
+.get((httpRequest, httpResponse) => {
     httpResponse.status(200).json(users)
 })
 
 
+
+// http://localhost:5000/api/users/1
+controller.route("/:id")
+.get((httpRequest, httpResponse) => {
+    if (httpRequest.user != undefined)
+        httpResponse.status(200).json(httpRequest.user)
+    else 
+        httpResponse.status(404).json()
+})
+.put((httpRequest, httpResponse) => {
+    if (httpRequest.user != undefined) {
+        users.forEach(user => {
+            if (user.id == httpRequest.user.id){
+                user.firstName = httpRequest.body.firstName ? httpRequest.body.firstName : user.firstName
+                user.lastName = httpRequest.body.lastName ? httpRequest.body.lastName : user.lastName
+                user.email = httpRequest.body.email ? httpRequest.body.email : user.email
+            }
+        })
+        httpResponse.status(200).json(httpRequest.user)
+    }
+    else 
+        httpResponse.status(404).json()
+})
+.delete((httpRequest, httpResponse) => {
+    if (httpRequest.user != undefined){
+        users = users.filter(user => user.id !== httpRequest.user.id)
+        httpResponse.status(204).json()
+    }
+    else 
+        httpResponse.status(404).json()
+})
 
 module.exports = controller 
